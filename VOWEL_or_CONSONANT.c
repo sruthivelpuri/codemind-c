f34ee03601fa006ec5aa9b9d0c274c11a1bb25f8
sruthivelpuri <147488298+sruthivelpuri@users.noreply.github.com>
2023-10-27 21:03:51 +0530

    #include<stdio.h> int main() {     char n;     scanf("%c",&n);     if(n=='a' or n=='A')     {         printf("VOWEL");     }     else if(n=='e' or n=='E')     {         printf("VOWEL");     }     else if(n=='i' or n=='I')     {         printf("VOWEL");     }     else if(n=='o' or n=='O')     {         printf("VOWEL");     }     else if(n=='u' or n=='U')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    if(n=='a' or n=='A')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='e' or n=='E')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='i' or n=='I')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='o' or n=='O')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='u' or n=='U')
+    {
+        printf("VOWEL");
+    }
+    else
+    {
+        printf("CONSONANT");
+    }
+}
