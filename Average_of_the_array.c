827e237132e2c14450754aca664091883ef8f218
sruthivelpuri <147488298+sruthivelpuri@users.noreply.github.com>
2023-12-18 15:08:27 +0530

    #include<stdio.h> int main() {     int n,i,sum=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         sum=sum+a[i];     }     float avg=(float)sum/n;     printf("%.2f",avg); }

---
+++ Average_of_the_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    float avg=(float)sum/n;
+    printf("%.2f",avg);
+}
