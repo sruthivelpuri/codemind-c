a060137e5853501ffdbff9203b3f5349c19d97b3
sruthivelpuri <147488298+sruthivelpuri@users.noreply.github.com>
2023-12-20 21:11:52 +0530

    #include<stdio.h> int main() {     int i,n,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j>=i)             {                 printf("* ");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

---
+++ Pattern_7.1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j>=i)
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+}
